b91daa72366b5bb64b7bbc893bab37c34724847b
Eddie van Rensburg <eddie@allthingsimprovement.com>
2023-04-28 11:18:44 +0200

    Updated code (arguing w/Champ, Main not rendering)

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,32 +17,8 @@ export default function Main() {
     return (
       <div className="main--div">
         <button onClick={generateQuote}>Generate Quote</button>
-        <QuoteBox quote={randomQuote} />
+        {currentQuote && <QuoteBox quote={currentQuote} />}
       </div>
     );
   }
 }
-
-// Work in step 3 (afer
-// props were passed down to QuoteBox) and then
-// create a seperate state update function as suggested
-// called handleGenerateQuote() to update state seperately from
-// the rest of the functionality of the code..
-
-//STEP 3: UPDATE THIS SECTION
-// function generateQuote() {
-//   const numRange = quotesData.length;
-//   const randomNumber = Math.floor(Math.random() * numRange);
-//   const randomQuote = quotesData[randomNumber];
-//   return randomQuote;
-// }
-
-//STEP 4: UPDATE THIS SECTION
-// return (
-//   <div className="main--div">
-//     <button onClick={() => setCurrentQuote(generateQuote)}>
-//       Generate Quote
-//     </button>
-//     {currentQuote && <QuoteBox quote={currentQuote} />}
-//   </div>
-// );
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -16,3 +16,4 @@ export default function QuoteBox(props) {
     </div>
   );
 }
+
