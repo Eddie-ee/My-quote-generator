40bd7c30b661204fcc8463a68f3c2f20ef952a52
Eddie van Rensburg <eddie@allthingsimprovement.com>
2023-05-08 13:35:35 +0200

    it works! update

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -6,7 +6,6 @@ import quotesData from "./quotesData";
 export default function QuoteBox(props) {
   return (
     <div>
-      <h2>Testing</h2>
       <div className="quoteBox--quote">
         <p>{props.quote.quote}</p>
       </div>
